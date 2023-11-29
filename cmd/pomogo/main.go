ec13c82d93d8f157a1e9d41b650a9b7a0ed9c64f
Milad Barzideh <m.barzideh@digikala.com>
2023-11-30 00:12:31 +0330

    feat(config): read the server port from configurations

--- cmd/pomogo/main.go
+++ cmd/pomogo/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/miladbarzideh/pomogo/configs"
 	"github.com/miladbarzideh/pomogo/internal/routes"
 )
 
 func main() {
 
+	config, err := configs.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
-
 	routes.Initialize(app)
-
 	app.Use(logger.New())
 
-	app.Listen(":3000")
+	err = app.Listen(fmt.Sprintf(":%v", config.Server.Port))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
