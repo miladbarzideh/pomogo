54d81d47b0e87bd13d9ee33b6189b9e877cfc38a
Milad Barzideh <m.barzideh@digikala.com>
2023-12-05 23:14:29 +0330

    refactor: rename to cfg

--- cmd/pomogo/main.go
+++ cmd/pomogo/main.go
@@ -5,26 +5,26 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/miladbarzideh/pomogo/configs"
-	"github.com/miladbarzideh/pomogo/internal/logger"
-	"github.com/miladbarzideh/pomogo/internal/routes"
+	"github.com/miladbarzideh/pomogo/internal/domain/routes"
+	"github.com/miladbarzideh/pomogo/internal/infra/config"
+	"github.com/miladbarzideh/pomogo/internal/infra/logger"
 	"go.uber.org/zap"
 )
 
 func main() {
 
-	config, err := configs.Load()
+	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	logger.Init(config)
-	logger.Logger.Info("starting pomogo", zap.String("app-version", config.Server.AppVersion))
+	logger.Init(cfg)
+	logger.Logger.Info("starting pomogo", zap.String("app-version", cfg.Server.AppVersion))
 
 	app := fiber.New()
 	routes.Initialize(app)
 
-	err = app.Listen(fmt.Sprintf(":%s", config.Server.Port))
+	err = app.Listen(fmt.Sprintf(":%s", cfg.Server.Port))
 	if err != nil {
 		log.Fatal(err)
 	}
