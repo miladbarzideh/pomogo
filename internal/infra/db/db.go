a3f1a32260f4ff289fc13d99c100a6300c1dfc5c
Milad Barzideh <m.barzideh@digikala.com>
2023-12-05 23:57:50 +0330

    feat(db): create database connection

---
+++ internal/infra/db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/miladbarzideh/pomogo/internal/infra/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewConnection(cfg config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		cfg.Postgres.Host,
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.DbName,
+		cfg.Postgres.Port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, errors.New("database connection failed")
+	}
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	if err = sqlDb.Ping(); err != nil {
+		return nil, errors.New("database ping error")
+	}
+
+	return db, nil
+}
