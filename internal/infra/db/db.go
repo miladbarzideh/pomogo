b0e96bad04b6353b741ec89a1d4c9b3c20c2f786
Milad Barzideh <m.barzideh@digikala.com>
2023-12-10 00:53:00 +0330

    refactor: add a method to build dsn

--- internal/infra/db/db.go
+++ internal/infra/db/db.go
@@ -9,14 +9,8 @@ import (
 	"gorm.io/gorm"
 )
 
-func NewConnection(cfg config.Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		cfg.Postgres.Host,
-		cfg.Postgres.User,
-		cfg.Postgres.Password,
-		cfg.Postgres.DbName,
-		cfg.Postgres.Port,
-	)
+func NewConnection(cfg *config.Config) (*gorm.DB, error) {
+	dsn := buildDsn(cfg)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -33,4 +27,14 @@ func NewConnection(cfg config.Config) (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func buildDsn(cfg *config.Config) string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		cfg.Postgres.Host,
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.DbName,
+		cfg.Postgres.Port,
+	)
 }
