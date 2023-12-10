6be025bb3f27ecd6039484fe74a4131df1eec173
Milad Barzideh <m.barzideh@digikala.com>
2023-12-10 22:28:34 +0330

    refactor: map handlers

---
+++ internal/app/handlers.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/miladbarzideh/pomogo/internal/domain/project/controller"
+	"github.com/miladbarzideh/pomogo/internal/domain/project/repository"
+	"github.com/miladbarzideh/pomogo/internal/domain/project/service"
+)
+
+func (s *Server) mapHandlers(app *fiber.App) error {
+
+	// Init repositories
+	projectRepository := repository.NewProjectRepository(s.db)
+
+	// Init services (use cases)
+	projectService := service.NewProjectService(projectRepository, s.logger)
+
+	// Init handlers
+	projectHandler := controller.NewProjectHandler(projectService, s.logger)
+
+	// Map routes
+	v1 := app.Group("/api/v1")
+	projectGroup := v1.Group("/projects")
+	controller.MapProjectRoutes(projectGroup, projectHandler)
+
+	return nil
+}
