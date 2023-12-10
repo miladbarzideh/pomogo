bb734076a4febe8bbbd1864470a0f88a56292337
Milad Barzideh <m.barzideh@digikala.com>
2023-12-10 22:26:43 +0330

    feat(project): create the project repository

---
+++ internal/domain/project/repository/project_repository.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"github.com/miladbarzideh/pomogo/internal/domain/project"
+	"gorm.io/gorm"
+)
+
+type repo struct {
+	db *gorm.DB
+}
+
+func NewProjectRepository(db *gorm.DB) project.Repository {
+	return &repo{db: db}
+}
+
+func (r repo) Create(project project.Project) (project.Project, error) {
+	result := r.db.Create(project)
+	return project, result.Error
+}
+
+func (r repo) GetByID(ID uint) (project.Project, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r repo) Update(project project.Project) (project.Project, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r repo) DeleteById(ID uint) error {
+	//TODO implement me
+	panic("implement me")
+}
